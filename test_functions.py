1517e377970fcbbd7eef73b01841dd0de6ad24cd
Nathan Weinberg <nathan2@stwmd.net>
2020-04-27 15:15:43 -0400

    Adding some additional tests

--- test_functions.py
+++ test_functions.py
@@ -1,6 +1,24 @@
 from functions import *
 
 
+def test_get_bugs_set():
+	mockers = {
+		'job1': {'bz': [0]},
+		'job2': {'bz': [123456]},
+		'job3': {'bz': [123456, 789123]}
+	}
+	assert get_bugs_set(mockers) == {0, 123456, 789123}
+
+
+def test_get_jira_set():
+	mockers = {
+		'job1': {'jira': [0]},
+		'job2': {'jira': ['RHOSINFRA-123']},
+		'job3': {'jira': ['RHOSINFRA-123', 'RHOSENTDFG-456']}
+	}
+	assert get_jira_set(mockers) == {0, 'RHOSINFRA-123', 'RHOSENTDFG-456'}
+
+
 def test_get_osp_version_func():
 	assert get_osp_version('DFG-all-unified-16_director-rhel-virthost-3cont_2comp_3ceph-ipv4-geneve-ceph-native-default') == '16'
 	assert get_osp_version('DFG-backup-restore-overcloud-OSP-16-3cont_2comp_3ceph-ipv4-monolithic-broken-node') == '16'
@@ -16,6 +34,29 @@ def test_get_osp_version_func():
 	assert get_osp_version('DFG-security-keystone-16_director-rhel-virthost-1cont_1comp-ipv4-geneve-lvm-containers') == '16'
 	assert get_osp_version('DFG-upgrades-updates-16-from-passed_phase1-HA-ipv4') == '16'
 	assert get_osp_version('DFG-all-unified-weekly-multijob') is None
+
+
+def test_has_blockers():
+	mockers = {
+		'job1': {'bz': [123456]},
+		'job2': {'jira': ['RHOSINFRA-123']},
+		'job3': {'other': {'name': 'this is a test name'}},
+		'job4': {'bz': [0]},
+		'job5': {'jira': [0]},
+		'job6': {'other': [0]},
+		'job7': {'owners': 'foo@bar.com'},
+		'job8': {'owners': 'foo@bar.com', 'bz': [123456], 'jira': ['RHOSINFRA-123']},
+		'job9': {}
+	}
+	assert has_blockers(mockers, 'job1') == True
+	assert has_blockers(mockers, 'job2') == True
+	assert has_blockers(mockers, 'job3') == True
+	assert has_blockers(mockers, 'job4') == False
+	assert has_blockers(mockers, 'job5') == False
+	assert has_blockers(mockers, 'job6') == False
+	assert has_blockers(mockers, 'job7') == False
+	assert has_blockers(mockers, 'job8') == True
+	assert has_blockers(mockers, 'job9') == False
 
 
 def test_percent_func():
