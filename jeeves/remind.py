2eadebe52a70e33c24e8e11d9d6eac3fef1cbe97
Nathan Weinberg <31703736+nathan-weinberg@users.noreply.github.com>
2021-05-04 10:16:07 -0400

    Added owner name to reminder body (#208)
    * Added owner name to reminder body
    
    * Added owner to filename as well

--- jeeves/remind.py
+++ jeeves/remind.py
@@ -130,6 +130,7 @@ def run_remind(config, blockers, server, header):
 			# generate HTML report
 			htmlcode = template.render(
 				header=header,
+				owner=owner,
 				rows=rows
 			)
 
@@ -161,7 +162,7 @@ def run_remind(config, blockers, server, header):
 
 			except Exception as e:
 				print("Error sending email reminder: {}\nHTML file generated".format(e))
-				generate_html_file(htmlcode, remind=True)
+				generate_html_file(htmlcode, remind=True, owner=owner)
 
 		else:
 			print("Owner {} has all passing jobs!".format(owner))
